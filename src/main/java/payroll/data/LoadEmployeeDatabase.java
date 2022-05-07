33d1e11e44f73567694dabdfc03fcc71b6aae04f
mrm5229 <mrm5229@gmail.com>
2022-05-07 15:28:17 -0700

    added all tutorial code

---
+++ src/main/java/payroll/data/LoadEmployeeDatabase.java
@@ -0,0 +1,24 @@
+package payroll.data;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import payroll.model.Employee;
+import payroll.repository.EmployeeRepository;
+
+@Configuration
+public class LoadEmployeeDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadEmployeeDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+        };
+    }
+}
